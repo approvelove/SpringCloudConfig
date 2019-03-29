0fb6c6aa13dc470d88cc9580cecdf3a3455ab215
李健 <1205247242@qq.com>
2019-03-29 16:31:56 +0800

    新建主项目

---
+++ src/main/java/com/lj/springcloudconfigcenter/SpringcloudConfigcenterApplication.java
@@ -0,0 +1,13 @@
+package com.lj.springcloudconfigcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigcenterApplication.class, args);
+	}
+
+}
