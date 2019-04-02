ce1d419b171391f717aca0f38e2f1c123dbb2871
李健 <1205247242@qq.com>
2019-04-02 13:44:55 +0800

    ...

--- config-client/src/main/java/com/lj/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/lj/configclient/ConfigClientApplication.java
@@ -1,13 +1,24 @@
 package com.lj.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	@Value("${user}")
+	String user;
+
+	@RequestMapping("/hi")
+	public String hi() {
+		return user;
+	}
 }
